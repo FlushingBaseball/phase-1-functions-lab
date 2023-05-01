d5699dd3d7a79a11d68760df67a5a98710456352
Sam Peck <fullstacksam@protonmail.com>
2023-05-01 15:59:03 -0400

    everything is calculated

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const hqLocation = 42;
+function distanceFromHqInBlocks(pickupLocation){
+    return Math.abs(pickupLocation - hqLocation);
+}
+
+
+function distanceFromHqInFeet(pickupLocation){
+   let evaluatedDistanceInBlocks =  distanceFromHqInBlocks(pickupLocation);
+
+
+//     let evaluatedDistanceInFeet = evaluatedDistanceInBlocks * 264;
+    return evaluatedDistanceInBlocks * 264;
+ }
+
+
+ function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264
+ }
+
+ function calculatesFarePrice(start, destination){
+   let evaluatedDistanceTravelledInFeet =  distanceTravelledInFeet(start, destination);
+    if (evaluatedDistanceTravelledInFeet<400){
+        return 0
+    }
+   else if (evaluatedDistanceTravelledInFeet >400 && evaluatedDistanceTravelledInFeet <= 2000){
+    return (evaluatedDistanceTravelledInFeet -400) * .02
+   }
+   else if (evaluatedDistanceTravelledInFeet >2000 && evaluatedDistanceTravelledInFeet < 2500){
+    return 25
+   }
+   else {
+    return `cannot travel that far`
+   }
+ }
